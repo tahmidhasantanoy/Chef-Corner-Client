c7210bb91caaa7307ba7277484dcd02f8881e131
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-05-02 18:41:54 +0600

    Environment setup done

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Bismillah</div>,
+  },
+]);
+
+export default router;
+
+// const Route = () => {
+//     return (
+//         <div>
+
+//         </div>
+//     );
+// };
+
+// export default Route;
---
+++ src/firebase/firebse.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG1PLhpqB2l5TxWsyr2zc-b5NcGgJWCS8",
+  authDomain: "chef-corner.firebaseapp.com",
+  projectId: "chef-corner",
+  storageBucket: "chef-corner.appspot.com",
+  messagingSenderId: "833774691924",
+  appId: "1:833774691924:web:8643633f5ad3c588202fc1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
